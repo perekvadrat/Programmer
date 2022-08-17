56a31fa2a349dd15e5afd5a8759dcc89622d235f
perekvadrat <perekvadrat@gmail.com>
2022-08-18 00:59:35 +0800

    bugfixes2

--- example006_Base/Program.cs
+++ example006_Base/Program.cs
@@ -9,8 +9,8 @@ int max = a;
 if (a > max) max = a;
 if (b > max) max = b;
 if (c > max) max = c;
-if (d > max) max = d;
-if (e > max) max = e;
+if (d > max)max = d;
+if (e> max) max = e;
 
 Console.Write ("max = ");
 Console.WriteLine(max);
