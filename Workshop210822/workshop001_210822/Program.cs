34ed3fdd0e1ff7ad14d767fce9a37a4832e61fd1
perekvadrat <perekvadrat@gmail.com>
2022-08-21 21:07:23 +0800

    add workshop2

---
+++ Workshop210822/workshop001_210822/Program.cs
@@ -0,0 +1,20 @@
+﻿int Sum(int x, int y)
+{
+    int result = x+y;
+    return result;
+}
+Console.Write("Enter first value: ");
+int x = int.Parse(Console.ReadLine);
+
+// 
+
+void FillArray(int[] array, int minimum, int maximum)
+{
+    foreach(var element in array)
+        Console.Write($"{element}");
+    Console.WriteLine();
+}
+
+//
+
+
---
+++ Workshop210822/workshop003_210822/Program.cs
@@ -0,0 +1,21 @@
+﻿// Определить номер четверти полоскости в которой находится точка XY
+
+int x = new Random().Next(-10, 10);
+Console.WriteLine(x);
+int y = new Random().Next(-10, 10);
+Console.WriteLine(y);
+
+if ((x >= 0) && (y >= 0))
+{
+    Console.WriteLine("Четверть I");
+else if ((x < 0) && (y > 0))
+    {
+    Console.WriteLine("Четверть II");
+    }
+else if ((x < 0) && (y < 0))
+    {
+    Console.WriteLine("Четверть III");
+    }
+}
+
+else Console.WriteLine("Четверть IV");
