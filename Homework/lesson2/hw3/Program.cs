8888e292070fdefe6f8f4b8b13caa33bd8754bab
perekvadrat <perekvadrat@gmail.com>
2022-08-28 02:45:45 +0800

    add lesson 2 hw3-4

---
+++ Homework/lesson2/hw3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+// Четверти нумеруются начиная с правой верхней и далее против часовой стрелки
+
+Console.Write("Введите номер четверти: ");
+int quarter = int.Parse(Console.ReadLine() ?? "0");
+
+if (quarter == 1)
+{
+    Console.WriteLine("x > 0, y > 0");
+}
+else if (quarter == 2)
+{
+    Console.WriteLine("x < 0, y > 0");
+}
+else if (quarter == 3)
+{
+    Console.WriteLine("x < 0, y < 0");
+}
+else Console.WriteLine("x > 0, y < 0");
---
+++ Homework/lesson2/hw4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Найти расстояние между точками в пространстве 2D/3D
+
+int xa = new Random().Next(-10, 10);
+Console.WriteLine($"xa = {xa}");
+int ya = new Random().Next(-10, 10);
+Console.WriteLine($"ya = {ya}");
+int xb = new Random().Next(-10, 10);
+Console.WriteLine($"xb = {xb}");
+int yb = new Random().Next(-10, 10);
+Console.WriteLine($"yb = {yb}");
+
+// AB = √(xb - xa)2 + (yb - ya)2
+
+double result = (Math.Sqrt(Math.Pow((xb - xa),2) + Math.Pow((yb - ya),2)));
+Console.WriteLine($"Растояние между точками = {result}");
