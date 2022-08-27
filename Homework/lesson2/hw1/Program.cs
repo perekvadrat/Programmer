78052d7ec702933600046c15fc6f23a6ffba3db2
perekvadrat <perekvadrat@gmail.com>
2022-08-28 01:55:34 +0800

    add lesson 2 hw1-2

---
+++ Homework/lesson2/hw1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+
+Console.WriteLine("Введите номер дня недели от 1 до 7");
+int numberA = int.Parse(Console.ReadLine() ?? "0");
+string[] week = new string[8] {"", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"};
+if (numberA >= 1 && numberA <= 7) {
+    Console.WriteLine(week[numberA]);
+    if (numberA >= 6 && numberA <= 7)
+    {
+        Console.WriteLine($"Это выходной!");
+    }
+    else
+    {
+        Console.WriteLine($"Это будний день");
+    }
+}
+else
+{
+    Console.WriteLine("Вы ввели неверное значение!");
+}
---
+++ Homework/lesson2/hw2/Program.cs
@@ -0,0 +1,18 @@
+﻿// По двум заданным числам проверять является ли одно квадратом другого
+
+Console.Write("Первое число: ");
+int numberA = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Второе число: ");
+int numberB = int.Parse(Console.ReadLine() ?? "0");
+if (numberA == numberB * numberB)
+{
+    Console.WriteLine("Первое число является квадратом второго!");
+}
+else if (numberB == numberA * numberA)
+{
+    Console.WriteLine("Второе число является квадратом первого!");
+}
+else
+{
+    Console.WriteLine("Ни одно из этих чисел НЕ является квадратом другого");
+}
