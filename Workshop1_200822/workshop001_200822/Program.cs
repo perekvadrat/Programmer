3556c4518b24e18dee4928fb0dcf5ecdd9e711a1
perekvadrat <perekvadrat@gmail.com>
2022-08-27 18:35:21 +0800

    sort folders again

--- Workshop1_200822/workshop001_200822/Program.cs
+++
@@ -1,4 +0,0 @@
-﻿Console.WriteLine("Введите число квадрат которого необходимо получить:");
-int number = int.Parse(Console.ReadLine());
-int result = number * number;
-Console.WriteLine(result);
--- Workshop1_200822/workshop005_200822/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿// Показать последнюю цифру трёхзначного числа
-
-int number = new Random().Next(100,1000);
-Console.WriteLine(number);
-int thirdNumber = number % 10;
-Console.WriteLine(thirdNumber);
